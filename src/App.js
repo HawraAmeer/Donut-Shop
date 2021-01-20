d16f236aec7df94c8cb2408924bfd3b78b140d50
HawraAmeer <hwra.ameer97@gmail.com>
2021-01-20 16:04:03 +0300

    2 Product List

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import photo from "./Pink-Donut.png";
 import "./App.css";
+import data from "./products";
 
 function App() {
+  const productsList = data.map((product) => {
+    return (
+      <td className="products">
+        <img className="image" src={product.image} alt="" />
+        <p>{product.name}</p>
+        <p>{`${product.price} BD / Piece`}</p>
+      </td>
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Welcome to Donut Shop</h1>
-        <h3>Where you can customize your Donut!</h3>
+        <h3>Pick up your Favorite Donut!</h3>
         <img src={photo} className="App-logo" alt="logo" />
       </header>
+      <table className="App App-header">
+        <tbody>
+          <tr>{productsList}</tr>
+        </tbody>
+      </table>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,22 @@
+const products = [
+  {
+    name: "Fireworks Donut",
+    price: 0.6,
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/assorted-glazed-donut-royalty-free-image-905879330-1538167169.jpg",
+  },
+  {
+    name: "Glazed Donut",
+    price: 0.4,
+    image:
+      "https://myrepublica.nagariknetwork.com/uploads/media/donuts2_20200102135901.jpg",
+  },
+  {
+    name: "Sugar Donut",
+    price: 0.5,
+    image:
+      "https://www.christinascucina.com/wp-content/uploads/2014/01/IMG_4471-720x540.jpg",
+  },
+];
+
+export default products;
