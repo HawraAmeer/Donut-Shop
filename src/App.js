d966f1a5e740091886c6aa417497f87e8011d223
HawraAmeer <hwra.ameer97@gmail.com>
2021-01-20 22:26:01 +0300

    2.Products List - update

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import photo from "./Pink-Donut.png";
-import "./App.css";
-import data from "./products";
+
+import products from "./products";
+
+const styles = {
+  body: { backgroundColor: "pink" },
+  donut: { margin: "20px" },
+  donutImage: { width: "200px", height: "150px" },
+  list: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+    color: "maroon",
+    textAlign: "center",
+  },
+  shopImage: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "50%",
+  },
+  text: { textAlign: "center", color: "maroon" },
+};
 
 function App() {
-  const productsList = data.map((product) => {
+  const productsList = products.map((product) => {
     return (
-      <td className="products">
-        <img className="image" src={product.image} alt="" />
-        <p>{product.name}</p>
-        <p>{`${product.price} BD / Piece`}</p>
-      </td>
+      <div style={styles.donut}>
+        <img src={product.image} style={styles.donutImage} alt={product.name} />
+        <h3>{product.name}</h3>
+        <p style={styles.text}>{`Price: ${product.price} BD / Piece`}</p>
+      </div>
     );
   });
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Donut Shop</h1>
-        <h3>Pick up your Favorite Donut!</h3>
-        <img src={photo} className="App-logo" alt="logo" />
-      </header>
-      <table className="App App-header">
-        <tbody>
-          <tr>{productsList}</tr>
-        </tbody>
-      </table>
+    <div style={styles.body}>
+      <h1 style={styles.text}>Welcome to Donut Shop</h1>
+      <h3 style={styles.text}>Pick up your Favorite Donut!</h3>
+      <img src={photo} style={styles.shopImage} alt="Donut!" />
+      <div style={styles.list}>{productsList}</div>
     </div>
   );
 }
